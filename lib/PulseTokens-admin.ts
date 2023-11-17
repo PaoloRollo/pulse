1fc12eedae55596436bf4c388310e0d34a38bd47
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2023-11-17 21:40:03 +0100

    wip eas

--- lib/PulseTokens-admin.ts
+++ lib/PulseTokens-admin.ts
@@ -1,9 +1,26 @@
 import { ethers } from "ethers"
-import { encodeFunctionData, formatUnits, parseUnits, createPublicClient, http, getContract, createWalletClient } from "viem"
+import { encodeFunctionData, formatUnits, parseUnits, createPublicClient, http, getContract, createWalletClient, parseAbi, encodeAbiParameters } from "viem"
 import { Chain, WalletClient } from "wagmi"
 import { baseGoerli } from 'viem/chains'
 import { PULSE_TOKEN_ABI } from '../utils/pulseTokenABI'
+import {  EAS_ABI } from '../utils/EAS'
+
 import { privateKeyToAccount } from 'viem/accounts' 
+
+interface AttestationRequestData {
+    recipient: string; // The recipient of the attestation.
+    expirationTime: number; // The time when the attestation expires (Unix timestamp).
+    revocable: boolean; // Whether the attestation is revocable.
+    refUID: string; // The UID of the related attestation.
+    data: string; // Custom attestation data.
+    value: number; // An explicit ETH amount to send to the resolver.
+}
+
+interface AttestationRequest {
+    schema: string; // The unique identifier of the schema.
+    data: AttestationRequestData; // The arguments of the attestation request.
+}
+
 
 const privateKey = process.env.PRIVATE_KEY;
 
@@ -31,5 +48,30 @@ async function mintNewPulseToken(account: string, id: number, amount: number, ne
 
     } catch (error) {
         console.error('Error calling mintAndSetUri:', error);
+    }
+}
+
+async function generateEAS(schema: string, account: string, data: string ) {
+    const attestationRequest: AttestationRequest = {
+        schema: schema,
+        data: {
+            recipient: account,
+            expirationTime: 0,
+            revocable: false,
+            refUID: "",
+            data: data, 
+            value: 0, 
+        },
+    };
+    try {
+        const generateAttestation = await client.writeContract({
+            address: '0x4200000000000000000000000000000000000021',
+            abi: EAS_ABI,
+            functionName: 'attest',
+            args: [attestationRequest],
+        });
+
+    } catch (error) {
+        console.error('Error calling attest:', error);
     }
 }
