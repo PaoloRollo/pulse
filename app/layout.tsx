9bb39c767e1319c554f1b7bee552b1cdf8199dbe
PaoloRollo <paolo.rollo1997@gmail.com>
2023-11-18 16:51:23 +0300

    feat: removed font.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,38 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
-import localFont from "next/font/local";
 
-const satoshi = localFont({
-  src: [
-    {
-      path: "../public/fonts/Satoshi-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Satoshi-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Satoshi-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Satoshi-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Satoshi-Black.woff2",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  display: "auto",
-  variable: "--font-satoshi",
-});
+// const satoshi = localFont({
+//   src: [
+//     {
+//       path: "../public/fonts/Satoshi-Regular.woff2",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     {
+//       path: "../public/fonts/Satoshi-Bold.woff2",
+//       weight: "700",
+//       style: "normal",
+//     },
+//     {
+//       path: "../public/fonts/Satoshi-Light.woff2",
+//       weight: "300",
+//       style: "normal",
+//     },
+//     {
+//       path: "../public/fonts/Satoshi-Medium.woff2",
+//       weight: "500",
+//       style: "normal",
+//     },
+//     {
+//       path: "../public/fonts/Satoshi-Black.woff2",
+//       weight: "900",
+//       style: "normal",
+//     },
+//   ],
+//   display: "auto",
+//   variable: "--font-satoshi",
+// });
 
 export const metadata: Metadata = {
   title: "Pulse",
@@ -46,11 +45,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`light overflow-x-hidden ${satoshi.variable} `}>
+    <html lang="en" className={`light overflow-x-hidden`}>
       <head>
         <link rel="icon" href="/logo-pulse.png" sizes="any" />
       </head>
-      <body className={`overflow-x-hidden !font-sansSerif`}>
+      <body className={`overflow-x-hidden`}>
         <Providers>{children}</Providers>
       </body>
     </html>
