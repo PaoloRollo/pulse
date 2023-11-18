b355cc85b97033c0bc1635cbd0adfe2bfc70e2ee
PaoloRollo <paolo.rollo1997@gmail.com>
2023-11-18 15:10:48 +0300

    feat: test fonts?

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,32 +6,33 @@ import localFont from "next/font/local";
 const satoshi = localFont({
   src: [
     {
-      path: "./fonts/Satoshi-Regular.woff2",
+      path: "../public/fonts/Satoshi-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/Satoshi-Bold.woff2",
+      path: "../public/fonts/Satoshi-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/Satoshi-Light.woff2",
+      path: "../public/fonts/Satoshi-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/Satoshi-Medium.woff2",
+      path: "../public/fonts/Satoshi-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/Satoshi-Black.woff2",
+      path: "../public/fonts/Satoshi-Black.woff2",
       weight: "900",
       style: "normal",
     },
   ],
-  display: "swap",
+  display: "auto",
+  variable: "--font-satoshi",
 });
 
 export const metadata: Metadata = {
@@ -45,11 +46,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="light overflow-x-hidden">
+    <html lang="en" className={`light overflow-x-hidden ${satoshi.variable} `}>
       <head>
         <link rel="icon" href="/logo-pulse.png" sizes="any" />
       </head>
-      <body className={satoshi.className}>
+      <body className={`overflow-x-hidden !font-sansSerif`}>
         <Providers>{children}</Providers>
       </body>
     </html>
