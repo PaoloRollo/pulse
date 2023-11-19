040bbb811690f1a3228f20775220ea652a33fe71
PaoloRollo <paolo.rollo1997@gmail.com>
2023-11-19 05:26:50 +0300

    feat: update address.

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@
 export const BASE_GOERLI_ENTRYPOINT_ADDRESS =
   "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
 // Sample NFT pulled from https://docs.base.org/guides/deploy-smart-contracts#verifying-the-smart-contract
-export const NFT_ADDRESS = "0xd4d65bb544776f1200ef59793c33c66175668455";
+export const NFT_ADDRESS = "0x2a28f918a5cd7bd839c3ecee69f2055ece456e7e";
 
 export const BASE_GOERLI_SCAN_URL = "https://goerli.basescan.org";
 export const BASE_GOERLI_PAYMASTER_URL = "https://paymaster.base.org";
